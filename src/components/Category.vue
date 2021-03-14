61836b65cdfab4177386d284ed9a001b2a8f0a47
Denisoed <denisoed@gmail.com>
2021-03-14 18:20:06 +0600

    fixed header active link

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -37,5 +37,115 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.single-products-catagory {
+  position: relative;
+  z-index: 1;
+  width: 33.33333%;
+  top: 0;
 
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    z-index: 10;
+
+    &::after {
+      -webkit-transition-duration: 250ms;
+      transition-duration: 250ms;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      content: '';
+      background-color: rgba(60, 60, 60, 0.7);
+      z-index: 1;
+      opacity: 0;
+      visibility: visible;
+    }
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .hover-content {
+    position: absolute;
+    top: 40px;
+    left: 40px;
+    z-index: 10;
+
+    .line {
+      width: 80px;
+      height: 3px;
+      background-color: #fbb710;
+      display: block;
+      margin-bottom: 15px;
+    }
+
+    p {
+      font-size: 16px;
+      color: #6d6d6d;
+      line-height: 1;
+      margin-bottom: 5px;
+      -webkit-transition-duration: 250ms;
+      transition-duration: 250ms;
+    }
+
+    h4 {
+      margin-bottom: 0;
+      -webkit-transition-duration: 250ms;
+      transition-duration: 250ms;
+    }
+  }
+
+  &:hover a::after, &:focus a::after {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &:hover .hover-content {
+    p, h4 {
+      color: #fff;
+    }
+  }
+
+  &:focus .hover-content {
+    p, h4 {
+      color: #fff;
+    }
+  }
+}
+
+@media only screen and (min-width: 992px) and (max-width: 1199px) {
+  .single-products-catagory {
+    width: 50%;
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+  .single-products-catagory {
+    width: 100%;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .single-products-catagory {
+    width: 100%;
+  }
+}
+
+@media only screen and (min-width: 576px) and (max-width: 767px) {
+  .single-products-catagory {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .single-products-catagory .hover-content {
+    top: 30px;
+    left: 30px;
+  }
+}
 </style>
--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -67,3 +67,190 @@ export default {
   components: { UILogo }
 }
 </script>
+
+<style lang="scss" scoped>
+.header-area {
+  -webkit-transition-duration: 500ms;
+  transition-duration: 500ms;
+  position: relative;
+  z-index: 1;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 320px;
+  flex: 0 0 320px;
+  width: 320px;
+  max-width: 320px;
+  background-color: #fff;
+  padding: 60px 75px;
+
+  &.bp-xs-on {
+    left: 0;
+  }
+
+  .nav-close {
+    -webkit-transition-duration: 500ms;
+    transition-duration: 500ms;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    top: 0;
+    right: 15px;
+    background-color: red;
+    text-align: center;
+    color: #fff;
+    line-height: 40px;
+    font-size: 12px;
+    cursor: pointer;
+    background-color: #fbb710;
+    display: none;
+
+    &:hover {
+      background-color: #131212;
+    }
+  }
+
+  .logo {
+    margin-bottom: 100px;
+    display: block;
+  }
+
+  .amado-nav li {
+    a {
+      font-size: 14px;
+      text-transform: uppercase;
+      position: relative;
+      z-index: 1;
+      padding: 20px 0;
+      display: block;
+      line-height: 0.9;
+      color: #131212;
+      font-weight: normal;
+
+      &::after {
+        -webkit-transition-duration: 500ms;
+        transition-duration: 500ms;
+        width: 30px;
+        height: 3px;
+        position: absolute;
+        top: 50%;
+        margin-top: -1.5px;
+        left: -150px;
+        background-color: #fbb710;
+        z-index: 10;
+        content: '';
+      }
+
+      &:hover, &:focus {
+        color: #fbb710;
+      }
+
+      &.nuxt-link-exact-active::after {
+        left: -75px;
+      }
+    }
+
+    &:hover a::after, &:focus a::after {
+      left: -75px;
+    }
+  }
+
+  .amado-btn-group .amado-btn {
+    width: 160px;
+    display: block;
+  }
+
+  .cart-fav-search a {
+    display: block;
+    text-transform: uppercase;
+    font-size: 16px;
+    padding: 15px 0;
+    color: #131212;
+    line-height: 1;
+
+    .search-nav {
+      padding-bottom: 0;
+    }
+
+    span {
+      color: #9e9e9e;
+    }
+
+    img {
+      margin-right: 10px;
+    }
+
+    &:hover, &:focus {
+      color: #fbb710;
+    }
+  }
+
+  .social-info a {
+    color: #b5b5b5;
+    display: inline-block;
+
+    i {
+      font-size: 18px;
+    }
+
+    &:hover, &:focus {
+      color: #fbb710;
+    }
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+  .header-area {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 280px;
+    flex: 0 0 280px;
+    width: 280px;
+    max-width: 280px;
+    padding: 50px;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .header-area {
+    -webkit-transition-duration: 500ms;
+    transition-duration: 500ms;
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 320px;
+    flex: 0 0 320px;
+    width: 320px;
+    max-width: 320px;
+    position: fixed;
+    z-index: 4000;
+    top: 0;
+    left: -350px;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    display: block;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .header-area .nav-close {
+    display: block;
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+  .header-area .amado-nav li {
+    &:hover a::after, &:focus a::after {
+      left: -50px;
+    }
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+  .header-area .amado-nav li a.nuxt-link-exact-active::after {
+    left: -50px;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .header-area .social-info {
+    margin-bottom: 75px;
+  }
+}
+</style>
